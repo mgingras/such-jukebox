2fabb48f713e6f01ad912cf42dd0362b2e18af54
Martin Gingras <martin.p.gingras@gmail.com>
2014-03-16 16:17:03 -0400

    Revert "Revert "Merge pull request #2 from mgingras/devin-new""
    This reverts commit 78d0ef9c5b1790846704e473094540e5ef70b1fa.

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-
+var fs = require('fs');
 var app = express();
+var database = require('./database');
+var testPopulator = require('./test-db-populator');
+testPopulator.populate();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,6 +23,8 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'OMGTHISISSOOOOSECRET'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,9 +38,31 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/hosting', routes.hosting);
 app.get('/joining', routes.joining);
-app.get('/party', routes.party);
+app.get('/party/:id', routes.party);
+app.post('/party/:id', routes.party);
+app.get('/party/:id/host', routes.hostParty);
+app.get('/party/:id/becomeGuest', routes.becomeGuest);
+app.post('/party/:id/voteSong', routes.partyVoteSong);
+app.post('/party/:id/newState', routes.newPartyState);
+app.post('/party/:id/updateCurrentSong', routes.updateCurrentSong);
+app.post('/party/:id/voteToSkipCurrentSong', routes.voteToSkipCurrentSong);
+
 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+/*fs.readFile('./db.json', 'utf8', function(err, data) {
+    if(err) {
+        console.error("Could not open file: %s", err);
+        return;
+    }
+    console.log(data);
+    fs.writeFile('./db.json', JSON.stringify({test: 1}), function(err) {
+        if(err) {
+            console.error("Could not write file: %s", err);
+        }
+    });
+});*/
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-
+var objects = require('../public/js/core-objects')
+var database = require('../database');
 /*
  * GET home page.
  */
@@ -16,5 +17,162 @@ exports.joining = function(req,res){
 }
 
 exports.party = function(req,res){
-  res.render('party', {title: 'Such Jukebox!' });
+    var id = req.params.id;
+    var hostPassword = req.body.hostPassword;
+
+    var party = database.getParty(id);
+
+    if( ! party ) {
+        console.log('User tried to access party that doesn\'t exist with ID ['+id+']')
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    var isHost = false;
+    if(req.session.host !== undefined && req.session.host[id]) {
+        console.log('Host is accessing party page for party with ID ['+id+']');
+        isHost = true;
+    }
+    
+    if( !isHost && hostPassword !== undefined && hostPassword === party.hostPassword ) {
+        console.log('Host is accessing party page for party with ID ['+id+']');
+        if(req.session.host === undefined) {
+            req.session.host = {};
+        }
+        req.session.host[id] = true;
+        isHost = true;
+    }
+
+    res.render('party', {
+    	title: 'Such Jukebox!', 
+    	party: party, 
+    	isHost: isHost
+    });
+}
+
+exports.hostParty = function(req,res){
+	var id = req.params.id;
+    var party = database.getParty(id);
+
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    res.render('hostParty', {
+    	title: 'Such Jukebox!',
+    	partyId: id
+    });
+}
+
+exports.becomeGuest = function(req, res) {
+    var id = req.params.id;
+    var party = database.getParty(id);
+
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    if(req.session.host)
+        req.session.host[id] = undefined;
+    res.send({});
+}
+
+
+exports.partyVoteSong = function(req,res){
+    var id = req.params.id;
+    var isVoteDown = req.body.isVoteDown;
+    var songQueueId = req.body.songQueueId;
+    var party = database.getParty(id);
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    if( ! songQueueId ) {
+    	res.send({error: 'You need to give a songQueueId'});
+        return;
+    }
+
+    if(isVoteDown === true || isVoteDown === 'true') {
+    	party.voteForSong(songQueueId, true);
+    } else {
+		party.voteForSong(songQueueId);
+    }
+
+    res.send({});
+}
+
+exports.newPartyState = function(req,res){
+    var id = req.params.id;
+    var party = database.getParty(id);
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    res.send({party: party});
+}
+
+
+exports.changedSongState = function(req,res){
+    var id = req.params.id;
+    var party = database.getParty(id);
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    res.send({party: party});
+}
+
+exports.updateCurrentSong = function(req,res) {
+    var id = req.params.id;
+    var party = database.getParty(id);
+    var oldSongId = req.body.oldSongId;
+    var newSongId = req.body.newSongId;
+
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    if(!req.session.host || ! req.session.host[id]){
+        console.log('Non host tried to change the song state')
+        res.send({error: 'You must be the host to do that'});
+        return;
+    }
+
+    if(oldSongId !== undefined) {
+        party.handleSongPlayed(oldSongId);
+    }
+
+    if(newSongId !== undefined) {
+        party.handleNewCurrentSong(newSongId);
+    }
+
+    res.send({party: party});
+}
+
+exports.voteToSkipCurrentSong = function(req, res) {
+    var id = req.params.id;
+    var party = database.getParty(id);
+    var songQueueId = req.body.songQueueId;
+
+    if( ! party ) {
+        res.send({error: 'Party does not exist'});
+        return;
+    }
+
+    if(!songQueueId){
+        res.send({error: 'You need to provide a song queue id'});
+        return;
+    }
+
+    if(party.currentSong !== undefined && ''+party.currentSong.id === songQueueId) {
+        party.currentSong.voteToSkip();
+    }
+
+    res.send({party: party});
 }
